6626135f6e492a636e60c31c78871dd8e90443ae
michael scotto <157407686+michael-scotto@users.noreply.github.com>
2024-02-04 17:38:37 -0500

    Add files via upload (#2)
    * Add files via upload
    
    * Rename Main.java to Main.java
    
    * Update Main.java

---
+++ Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.printf("Hello World!\n");
+
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 0; j < 2; j++) {
+
+            }
+            System.out.println("i = " + i);
+        }
+    }
+}
