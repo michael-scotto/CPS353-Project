6e0042d4a54635ba174ea42421e6a5f9c837349a
michael scotto <157407686+michael-scotto@users.noreply.github.com>
2024-02-11 16:03:27 -0500

    move storeData

--- Interface files/ComputeJobs.java
+++ Interface files/ComputeJobs.java
@@ -5,9 +5,6 @@ public interface ComputeJobs {
     //will do nessesary computations to find palindrome...
     void calculation(int input);
 
-    //store each iteration
-    void storeData(List data);
-
     //Store count of each iteration
     void storeIterationCount();
 }
