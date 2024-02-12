cc4c71f41968c84259eee73daabd146624d108ea
michael scotto <157407686+michael-scotto@users.noreply.github.com>
2024-02-12 00:50:13 -0500

    create smoke test files for each API
    not completely done yet, dont know what im doing

---
+++ test/UserAPITest.java
@@ -0,0 +1,13 @@
+package test;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import static org.mockito.Mockito.*;
+public class UserAPITest {
+    @Test
+    public void testDelimeter() {
+        Mockito delim = Mockito.mock(Mockito.class);
+
+    }
+}
