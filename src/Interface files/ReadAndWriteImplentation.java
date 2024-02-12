c334fdd552f798ed5b661c71bf23cc29d0a0c27f
michael scotto <157407686+michael-scotto@users.noreply.github.com>
2024-02-11 23:00:45 -0500

    create empty implementations for each API

---
+++ src/Interface files/ReadAndWriteImplentation.java
@@ -0,0 +1,9 @@
+package src;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ReadAndWriteImplentation implements ReadAndWrite {
+    int input;
+    List<Integer> iterationStorage = new ArrayList<Integer>();
+}
