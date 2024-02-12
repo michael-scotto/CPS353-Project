4d518f88ea84b3bdb79a49d4447964590a8d7054
Daniele-Persichetti <157838597+Daniele-Persichetti@users.noreply.github.com>
2024-02-12 15:29:44 -0500

    Update UserAPIImplementation.java
    Empty implementation of UserAPI

--- src/Interface files/UserAPIImplementation.java
+++ src/Interface files/UserAPIImplementation.java
@@ -2,10 +2,32 @@ package src;
 import java.util.ArrayList;
 import java.util.List;
 
-public class UserAPIImplementation implements UserAPI{
-    int input;
-    List<Integer> iterationStorage = new ArrayList<Integer>();
-    String delim1;
-    String delim2;
-    String outputDelimeterResult;
+public class UserAPIImplementation implements UserAPI {
+    private DataStoreAPI dataStoreAPI; // Instance variable for DataStoreAPI
+    
+    public UserAPIImplementation(DataStoreAPI dataStoreAPI) {
+        this.dataStoreAPI = dataStoreAPI;
+    }
+    
+    public void setInput(int input) {
+        // No implementation yet
+    }
+
+    public String inputDelimiter(String delim1) {
+        // No implementation yet
+        return "";
+    }
+
+    public String outputDelimiter(String delim2) {
+        // No implementation yet
+        return "";
+    }
+
+    public void executeJob(String outputDelimeterResult) {
+        // No implementation yet
+    }
+
+    public void storeData(List data) {
+        // No implementation yet
+    }
 }
