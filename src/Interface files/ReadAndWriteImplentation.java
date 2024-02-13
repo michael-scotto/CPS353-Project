3c1a82bcb11d1eed22a1f6ab417ae3c7d8741035
Daniele-Persichetti <157838597+Daniele-Persichetti@users.noreply.github.com>
2024-02-13 15:40:30 -0500

    Update ReadAndWriteImplementation.java
    Added implementation for ReadAndWrite

--- src/Interface files/ReadAndWriteImplentation.java
+++ src/Interface files/ReadAndWriteImplentation.java
@@ -3,7 +3,23 @@ package src;
 import java.util.ArrayList;
 import java.util.List;
 
-public class ReadAndWriteImplentation implements ReadAndWrite {
+public class ReadAndWriteImplementation implements ReadAndWrite {
+    private DatabaseConnection databaseConnection;
+    //Constructor for connecting to future database
+    public ReadAndWriteImplementation(DatabaseConnection databaseConnection){
+        this.databaseConnection = databaseConnection
+    }
+    //Writes to the database. int input is provisionary
+    public void write(int input){
+    
+    }
+    //Reads from the database
+    public String read(){
+    return "";
+    }
+    //we might need more methods to retrieve specific things from the database.
+
+    //Not sure if we need these
     int input;
     List<Integer> iterationStorage = new ArrayList<Integer>();
 }
