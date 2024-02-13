59ec8d3841f7ba47b8a91f3fb0f75b82a06d4cf3
michael scotto <157407686+michael-scotto@users.noreply.github.com>
2024-02-13 14:34:13 -0500

    Add integration test

---
+++ test/TestCalcComputeEngine.java
@@ -0,0 +1,30 @@
+package test;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TestCalcComputeEngine {
+    @Test
+    public void CalcComputeEngine() {
+
+        TestInput input = new TestInput();
+
+        List<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(10);
+        list.add(25);
+
+        input.TestInput(list);
+
+        TestOutput output = new TestOutput();
+        output.TestOutput(list);
+
+        TestDataStore dataStore = new TestDataStore();
+        dataStore.performOperation(list);
+
+
+    }
+
+}
