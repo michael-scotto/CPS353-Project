159b5224da58690dfb138d065161cce1aadf1722
Daniele-Persichetti <157838597+Daniele-Persichetti@users.noreply.github.com>
2024-02-13 16:50:32 -0500

    Update TestInput.java (#19)
    * Update TestInput.java
    
    added getInput() and setInput()
    
    * Update TestInput.java
    
    forgot to set input private

--- test/TestInput.java
+++ test/TestInput.java
@@ -2,9 +2,16 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class TestInput {
-    List<Integer> input;
+    private List<Integer> input;
     
     public TestInput(List<Integer> list) {
         this.input = list;
+    }
+    public List<Integer> getInput(){
+        return input;
+    }
+
+    public setInput(List<Integer> input){
+        this.input = input;
     }
 }
