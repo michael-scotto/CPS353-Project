dbf8f0fc17cafc75345596bbb6d9caacee0a1c86
michael scotto <157407686+michael-scotto@users.noreply.github.com>
2024-02-13 16:09:57 -0500

    add notes to clarify what is being done

--- test/TestCalcComputeEngine.java
+++ test/TestCalcComputeEngine.java
@@ -11,16 +11,20 @@ public class TestCalcComputeEngine {
 
         TestInput input = new TestInput();
 
+        //add 1, 10, 25 to arraylist
         List<Integer> list = new ArrayList<>();
         list.add(1);
         list.add(10);
         list.add(25);
 
+        //calling testInput with list to "test" TestInput class with a specific inital input
         input.TestInput(list);
 
+        //calling TestOutput with same list to be tested with a specific intial input
         TestOutput output = new TestOutput();
         output.TestOutput(list);
-
+        
+        //throwing list into dataStore for possible future use of list
         TestDataStore dataStore = new TestDataStore();
         dataStore.performOperation(list);
 
