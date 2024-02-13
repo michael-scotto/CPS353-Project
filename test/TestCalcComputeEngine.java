9b26f4c29d381cb7524a025f9a31312b42550e10
michael scotto <157407686+michael-scotto@users.noreply.github.com>
2024-02-13 14:35:21 -0500

    Update TestCalcComputeEngine.java

--- test/TestCalcComputeEngine.java
+++ test/TestCalcComputeEngine.java
@@ -7,7 +7,7 @@ import java.util.List;
 
 public class TestCalcComputeEngine {
     @Test
-    public void CalcComputeEngine() {
+    public void calcComputeEngine() {
 
         TestInput input = new TestInput();
 
