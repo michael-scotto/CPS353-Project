aba439892db0f85711cd665cc3441fae2d09989e
Daniele-Persichetti <157838597+Daniele-Persichetti@users.noreply.github.com>
2024-02-13 16:39:33 -0500

    Update TestDataStore.java
    Added the implementation of ReadAndWrite, and DataStoreAPI. 
    
    Need to add setters and getters to TestInput.java and TestOutput.java

--- test/TestDataStore.java
+++ test/TestDataStore.java
@@ -1,7 +1,52 @@
 import java.util.ArrayList;
 import java.util.List;
 
-public class TestDataStore {
+public class TestDataStore implements DataStoreAPI, ReadAndWrite {
+    private TestInput testInput;
+    private TestOutput testOutput;
+
+    public TestDataStore(TestInput testInput, TestOutput testOutput){
+        this.testInput = testInput;
+        this.testOutput = testOutput;
+    }
+    
+    public void write(int input) {
+        // Store value in test input
+        testInput.getInput().add(value);
+    }
+
+    public String read() {
+        return "";
+    }
+
+    public void storeIterations() {
+    }
+
+    public int getIterations(int input) {
+        return 0;
+    }
+
+    //Read input
+    public void readInput(List<Integer> data){
+        testInput.getInput(data);
+    }
+    
+    // Write data to the test output
+    public void writeOutput(List<String> data) {
+        testOutput.setOutput(data);
+    }
+
+    // Get test input
+    public TestInput getTestInput() {
+        return testInput;
+    }
+
+    // Get test output
+    public TestOutput getTestOutput() {
+        return testOutput;
+    }
+    
+    //not sure here
     public List<String> performOperation(List<Integer> input){
         List<String> output;
         output = new ArrayList<>();
