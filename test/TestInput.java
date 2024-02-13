693549f2b5bdb0026110000a080feb3a65e42893
michael scotto <157407686+michael-scotto@users.noreply.github.com>
2024-02-12 20:31:04 -0500

    combine input and output

--- test/TestInput.java
+++ test/TestInput.java
@@ -1,10 +1,14 @@
 import java.util.ArrayList;
 import java.util.List;
 
-public class TestInput {
+public class TestInputOutput {
     List<Integer> input;
-
+    List<String> output;
+    
     public TestInput() {
         this.input = new ArrayList<>();
+    }
+    public TestOutput(){
+        this.output = new ArrayList<>();
     }
 }
