6953646291bcfea1ab58e2d13059d66823cd8687
Daniele-Persichetti <157838597+Daniele-Persichetti@users.noreply.github.com>
2024-02-13 16:04:36 -0500

    Update ReadAndWriteImplementation.java (#16)
    * Update ReadAndWriteImplementation.java
    
    Added implementation for ReadAndWrite
    
    * fix typo in file name
    
    * checkstyle fix?
    
    * checkstyle fix?
    
    ---------
    
    Co-authored-by: michael scotto <157407686+michael-scotto@users.noreply.github.com>

--- src/Interface files/ReadAndWriteImplentation.java
+++
@@ -1,9 +0,0 @@
-package src;
-
-import java.util.ArrayList;
-import java.util.List;
-
-public class ReadAndWriteImplentation implements ReadAndWrite {
-    int input;
-    List<Integer> iterationStorage = new ArrayList<Integer>();
-}
