8a6b52b142b6e1ac017c0c023b26c0a0eaa7e805
michael scotto <157407686+michael-scotto@users.noreply.github.com>
2024-02-20 18:43:03 -0500

    Update TestDataStore.java
    used sample test

--- test/TestDataStore.java
+++ test/TestDataStore.java
@@ -1,65 +1,18 @@
-import java.util.ArrayList;
-import java.util.List;
 
 public class TestDataStore implements DataStoreAPI, ReadAndWrite {
-    private TestInput testInput;
-    private TestOutput testOutput;
-
-    public TestDataStore(TestInput testInput, TestOutput testOutput){
-        this.testInput = testInput;
-        this.testOutput = testOutput;
-    }
     
-    public void write(int input) {
-        // Store value in test input
-        testInput.getInput().add(value);
-    }
+    @Override
+	public Iterable<Integer> read(InputConfig input) {
+		// Test code is allowed to assume it's getting the right types; this will fail with a ClassCastException if it gets
+		// another type of input. For production code, we'd want some better user input validation
+		return ((TestInput)input).getInput();
+	}
 
-    public String read() {
-        return "";
-    }
-
-    public void storeIterations() {
-    }
-
-    public int getIterations(int input) {
-        return 0;
-    }
-
-    //Read input
-    public void readInput(List<Integer> data){
-        testInput.getInput(data);
-    }
-    
-    // Write data to the test output
-    public void writeOutput(List<String> data) {
-        testOutput.setOutput(data);
-    }
-
-    // Get test input
-    public TestInput getTestInput() {
-        return testInput;
-    }
-
-    // Get test output
-    public TestOutput getTestOutput() {
-        return testOutput;
-    }
-    
-    //not sure here
-    public List<String> performOperation(List<Integer> input){
-        List<String> output;
-        output = new ArrayList<>();
-        //asssuming each item of the output list is the # of iterations
-        for (int i=0;i<input.size();i++){
-            if (input.get(i)==1){
-                output.add("0");
-            } else if (input.get(i)==10){
-                output.add("1");
-            } else if (input.get(i)==25){
-                output.add("1");
-            }
-        }
-        return output;
-    }
+	@Override
+	public WriteResult appendSingleResult(OutputConfig output, String result) {
+		// Test code is allowed to assume it's getting the right types; this will fail with a ClassCastException if it gets
+		// another type of input. For production code, we'd want some better user input validation
+		((TestOutput)output).getOutput().add(result);
+		return () -> WriteResult.WriteResultStatus.SUCCESS;
+	}
 }
