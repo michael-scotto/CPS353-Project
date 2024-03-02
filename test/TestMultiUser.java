c0ff0986a160c8a2713454674a2ea93e547caec5
michael scotto <157407686+michael-scotto@users.noreply.github.com>
2024-03-02 01:16:53 -0500

    checkstyle
    less dumb

--- test/TestMultiUser.java
+++ test/TestMultiUser.java
@@ -72,9 +72,9 @@ public class TestMultiUser {
         Assert.assertEquals(singleThreaded, multiThreaded);
     }
 
-    private List<String> loadAllOutput(String prefix, int nThreads) throws IOException {
+    private List<String> loadAllOutput(String prefix, int threads) throws IOException {
         List<String> result = new ArrayList<>();
-        for (int i = 0; i < nThreads; i++) {
+        for (int i = 0; i < threads; i++) {
             File multiThreadedOut =
                     new File(prefix + i);
             result.addAll(Files.readAllLines(multiThreadedOut.toPath()));
