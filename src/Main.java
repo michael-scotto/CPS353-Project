ee02173dc0dfbd2c5e51de2857427163f2352dfc
CassRechner <rechnerh1@newpaltz.edu>
2024-05-10 13:24:37 -0400

    Started the skeleton of the system for reading files.

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,32 @@
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter number or file: ");
-        int num = sc.nextInt();
-        LychrelComputation.lychrelCheck(num);
-        System.out.println(num); //debug line
-        //let it take in a number or a string (string is filename)
+        System.out.println("Are you inputting a number [0] or a filename [1]? Then, followed by a space, put the integer or the filename you want to be used.\nInput '0' or '1', followed by the number or filename:");
+        int b = sc.nextInt();
+        if (b==0){
+            int num = sc.nextInt();
+            //LychrelComputation.lychrelCheck(num);
+            System.out.println(num); //debug line
+        } else if (b==1){
+            String name = sc.nextLine();
+            File file = new File(name);
+            try {
+                sc = new Scanner(file);
+            } catch (FileNotFoundException e) {
+                throw new RuntimeException(e);
+            }
+            while (sc.hasNextInt()){
+                int num = sc.nextInt();
+                //LychrelComputation.lychrelCheck(num);
+                System.out.println(num); //debug line
+            }
+        } else {
+            System.out.println("Invalid input, you have to put 0 or 1.");
+        }
+
     }
 }
