915646a65b9be09ed0cab4a1d5b8ec09f557e257
michael scotto <157407686+michael-scotto@users.noreply.github.com>
2024-05-10 22:20:32 -0400

    Typo (#51)
    * Fixed typo
    
    * Update Lychrel.proto

--- src/LychrelComputationServiceImpl.java
+++ src/LychrelComputationServiceImpl.java
@@ -1,6 +1,6 @@
 import grpcDataService.Lychrel;
 
-public class LychrelComputationServiceImpl extends grpcDataService.LychrelComputatioSnerviceGrpc.LychrelComputatioSnerviceImplBase{
+public class LychrelComputationServiceImpl extends grpcDataService.LychrelComputationServiceGrpc.LychrelComputationServiceImplBase{
     public void checkLychrelNumber(grpcDataService.Lychrel.LychrelRequest request,
        io.grpc.stub.StreamObserver<grpcDataService.Lychrel.LychrelResponse> responseObserver) {
         Lychrel.LychrelResponse response = Lychrel.LychrelResponse.newBuilder().setIsLychrel(LychrelComputation.lychrelCheck(request.getNumber())).build();
